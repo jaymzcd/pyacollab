22475e03f139317ef0fd409675588a8b613019d9
jaymz campbell <jaymzcd@googlemail.com>
2011-02-15 22:21:08 +0000

    flesh out setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pyacollab',
+    version='0.2.0',
+    description='Library to interact with Active Collab, a Basecamp like PM tool',
+    author='Jaymz Campbell',
+    author_email='jaymz@jaymz.eu',
+    url='https://github.com/jaymzcd/pyacollab',
+    packages=['activecollab',],
+)
